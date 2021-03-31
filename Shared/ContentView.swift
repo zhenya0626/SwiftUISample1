57bc01552e265236f7671efd88a5ea1f142eb9b6
zhenya0626 <riana0626@yahoo.co.jp>
2021-04-01 01:40:48 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 内間理亜奈 on 2021/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MinecraftLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinecraftLoginApp.swift
+//  Shared
+//
+//  Created by 内間理亜奈 on 2021/04/01.
+//
+
+import SwiftUI
+
+@main
+struct MinecraftLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
